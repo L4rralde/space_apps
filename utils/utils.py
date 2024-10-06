88fcbabd6add21d80940b8ca1c31fdf785616053
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-10-06 17:59:52 -0600

    Using not hardcoded paths

--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,16 @@
+import os
+
 import pandas as pd
 import numpy as np
 from obspy import read
 
-DATA_DIR = "../data/lunar/training/data/S12_GradeA/"
-CATALOGS_PATH = "../data/lunar/training/catalogs/"
-CATALOG_PATH = "../data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv"
+DATA_DIR = f"{os.environ['SPACE']}/data/lunar/training/data/S12_GradeA/"
+CATALOGS_PATH = f"{os.environ['SPACE']}/data/lunar/training/catalogs/"
+CATALOG_PATH = f"{os.environ['SPACE']}/data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv"
 
 
 def get_data() -> pd.DataFrame:
-    fpath = "../data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv"
+    fpath = f"{os.environ['SPACE']}/data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv"
     cat = pd.read_csv(fpath)
     test_filename = cat.iloc[6].filename
     csv_file = f'{DATA_DIR}/{test_filename}.csv'
