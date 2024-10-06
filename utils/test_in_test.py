b95355c38dc0f1edce0a87c3bfb790e99231350e
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-10-06 14:37:45 -0600

    Passing percentile

--- utils/test_in_test.py
+++ utils/test_in_test.py
@@ -8,7 +8,6 @@ sys.path.append(f"{SPACE}/model")
 from codigo import Model
 
 
-
 LUNAR_TEST_DIR = f"{SPACE}/data/lunar/test/data/"
 MARS_TEST_DIR = f"{SPACE}/data/mars/test/data/"
 
@@ -17,7 +16,7 @@ def main():
     model = Model()
     mars_glob = glob.glob(f"{MARS_TEST_DIR}/*.mseed")
     for f in mars_glob:
-        model.predict(f)
+        model.predict(f, percentile=65)
         model.plot()
 
 if __name__ == '__main__':
