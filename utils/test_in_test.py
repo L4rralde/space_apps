a6d7e03b149043e5852041bbd79cc71bc216f7a7
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-10-06 02:49:15 -0600

    testing on mars

---
+++ utils/test_in_test.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import glob
+
+SPACE = os.environ["SPACE"]
+
+sys.path.append(f"{SPACE}/model")
+from codigo import Model
+
+
+
+LUNAR_TEST_DIR = f"{SPACE}/data/lunar/test/data/"
+MARS_TEST_DIR = f"{SPACE}/data/mars/test/data/"
+
+
+def main():
+    model = Model()
+    mars_glob = glob.glob(f"{MARS_TEST_DIR}/*.mseed")
+    for f in mars_glob:
+        model.predict(f)
+        model.plot()
+
+if __name__ == '__main__':
+    main()
