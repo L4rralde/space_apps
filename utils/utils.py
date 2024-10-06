01baa385bd6eaa967eccd78a9f960139dbc3f52b
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-10-06 01:05:39 -0600

    Enhancing utils

--- utils/utils.py
+++ utils/utils.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 from obspy import read
 
-data_directory = '../data/lunar/training/data/S12_GradeA/'
+DATA_DIR = "../data/lunar/training/data/S12_GradeA/"
+CATALOGS_PATH = "../data/lunar/training/catalogs/"
+CATALOG_PATH = "../data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv"
+
 
 def get_data() -> pd.DataFrame:
     fpath = "../data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv"
     cat = pd.read_csv(fpath)
     test_filename = cat.iloc[6].filename
-    csv_file = f'{data_directory}{test_filename}.csv'
+    csv_file = f'{DATA_DIR}/{test_filename}.csv'
     df = pd.read_csv(csv_file).sort_values(by=['time_rel(sec)'])
 
     return (df['time_rel(sec)'], df['velocity(m/s)'])
